7ec885b44ebca3a054e332b7ba89d721a028cfc9
EzequielMovia <136653654+EzequielMovia@users.noreply.github.com>
2024-05-30 20:49:17 -0300

    creamos aplicacion del clima con react

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { AplicacionClima } from './AplicacionClima'
+import './style/styleClima.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AplicacionClima />
+  </React.StrictMode>,
+)
